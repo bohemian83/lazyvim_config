8289a6293987a98dbac622edd44e28626b159950
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2024-04-04 22:53:12 +0200

    updated lazy plugins

--- lua/plugins/my_plugins.lua
+++ lua/plugins/my_plugins.lua
@@ -3,6 +3,25 @@ return {
     "nvim-neotest/nvim-nio",
   },
   {
+    "cameron-wags/rainbow_csv.nvim",
+    config = true,
+    ft = {
+      "csv",
+      "tsv",
+      "csv_semicolon",
+      "csv_whitespace",
+      "csv_pipe",
+      "rfc_csv",
+      "rfc_semicolon",
+    },
+    cmd = {
+      "RainbowDelim",
+      "RainbowDelimSimple",
+      "RainbowDelimQuoted",
+      "RainbowMultiDelim",
+    },
+  },
+  {
     "echasnovski/mini.move",
     lazy = false,
     version = false,
